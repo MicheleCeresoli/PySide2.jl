1d6d0ece87cd65fd92ee2982213e5cf0bdaeda00
MicheleCeresoli <85893254+MicheleCeresoli@users.noreply.github.com>
2022-08-25 15:55:32 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(;
     modules=[PySide2],
     authors="Michele Ceresoli <michele.ceresoli@mail.polimi.it>",
     sitename="PySide2.jl",
-
+    repo="https://micheleceresoli.github.io/PySide2.jl/blob/{commit}{path}#{line}",
     format = Documenter.HTML(canonical="https://micheleceresoli.github.io/PySide2.jl/stable/"),
 
     pages=[
